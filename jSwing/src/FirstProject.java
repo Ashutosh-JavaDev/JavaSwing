f461c616c3c704ef5dc719ddfb9890e89ca00ce0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-12 14:11:44 +0530

    String Maximum

--- jSwing/src/FirstProject.java
+++ jSwing/src/FirstProject.java
@@ -1,5 +1,12 @@
-public class FirstProject {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javax.swing.JFrame;
+public class FirstProject extends JFrame {
+    public void javaSwing() {
+        setLayout(null);
+        setSize(400, 400);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+       FirstProject ob= new FirstProject();
     }
 }
