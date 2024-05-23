71579d6ff08e1597008c119bf8fba84686701f3c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-23 15:46:57 +0530

    Button in Jswing

--- jSwing/src/Button.java
+++ jSwing/src/Button.java
@@ -5,5 +5,8 @@ public class Button {
 
     public Button(){
         frame=new JFrame("This is the main Title");
+        frame.setLayout(null);
+        frame.setSize(400,400);
+        frame.setVisible(true);
     }
 }
