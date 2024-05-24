235bbedbfcaaad0a22c51c20c04be68502a74145
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-24 13:00:19 +0530

    password in Jswing

--- jSwing/src/Password.java
+++ jSwing/src/Password.java
@@ -4,10 +4,14 @@ public class Password {
     JFrame jframe;
     JLabel jlabel;
     JPasswordField jpasswordfield;
-
+    JButton jbutton;
     public Password() {
         jframe=new JFrame("This Is Password Block");
-        jlabel=new JLabel("Password");
 
+        jlabel=new JLabel("Password");
+        jbutton=new JButton("Check Password");
+        jframe.setLayout(null);
+        jframe.setSize(400,400);
+        jframe.setVisible(true);
     }
 }
