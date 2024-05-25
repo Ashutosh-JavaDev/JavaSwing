0c078b16676527ecbf39ee7dfc178c6d24e8e890
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-25 12:14:58 +0530

    Checkbox in Jswing

--- jSwing/src/checkbox.java
+++ jSwing/src/checkbox.java
@@ -1,8 +1,9 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 public class checkbox {
-    public checkbox(){
+    public checkbox() {
 
     }
 }
