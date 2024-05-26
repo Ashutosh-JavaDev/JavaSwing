e5cce66070a971083078642f183916b7876b3f63
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-26 12:59:22 +0530

    RadioButton  in Jswing

--- jSwing/src/Radiobutton.java
+++ jSwing/src/Radiobutton.java
@@ -2,5 +2,11 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 public class Radiobutton {
-    
+    JFrame frame;
+    JLabel label;
+    JButton button;
+    JRadioButton radiobutton;
+    public Radiobutton(){
+        
+    }
 }
