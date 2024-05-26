22784a97f9ff4b1a1936fe08df240ad64bc5b906
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-26 13:08:33 +0530

    RadioButton  in Jswing

--- jSwing/src/Radiobutton.java
+++ jSwing/src/Radiobutton.java
@@ -1,17 +1,23 @@
 import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 public class Radiobutton {
     JFrame frame;
     JLabel label;
     JButton button;
-    JRadioButton Male,Female;
-    public Radiobutton(){
-        frame=new JFrame("Check Gender");
-        label=new JLabel("Gender");
-        label.setBounds(50,100,100,30);
+    JRadioButton Male, Female;
+
+    public Radiobutton() {
+        frame = new JFrame("Check Gender");
+        label = new JLabel("Gender");
+        label.setBounds(10, 50, 50, 30);
+        Male = new JRadioButton("Male");
+        Male.setBounds(40, 100, 100, 30);
+        button = new JButton("Click");
+        button.setBounds(0, 0, 0, 0);
         frame.setLayout(null);
-        frame.setSize(400,400);
+        frame.setSize(400, 400);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 
