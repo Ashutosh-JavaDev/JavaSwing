65fbba84efa8376ac4097db50353ca9f75935f6f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-28 21:10:19 +0530

    Combobox in Jswing

--- jSwing/src/combogbox.java
+++ jSwing/src/combogbox.java
@@ -5,8 +5,10 @@ import java.awt.event.ActionListener;
 public class combogbox {
     public combogbox() {
         JFrame jframe=new JFrame("Combo Box");
-        JLabel jlabel=new JLabel();
+        JLabel jlabel=new JLabel("Select City");
+        jlabel.setBounds(15,50,100,20);
         JButton jbutton=new JButton("Click Me");
+        jbutton.setBounds(100,100,100,30);
         String city[]={"Bhagalpur, Ranchi, Bhopal, Delhi, Pune"};
         JComboBox jcombox=new JComboBox(city); 
         jframe.setLayout(null);
